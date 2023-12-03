ecd64ee364323b885195f57cf87e768c725ec7f5
formegusto <formegusto@gmail.com>
2023-12-03 18:15:28 +0900

    iterationProtocolExample init

---
+++ src/examples/iterationProtocolExam.ts
@@ -0,0 +1,9 @@
+import { RangeIterable } from "../models";
+
+const range = new RangeIterable(10, 100, 10);
+for (let r of range) {
+  console.log(r); // for-of : 10 ,,, 90
+}
+console.log(...range); // spread syntax : 10 ,,, 90
+const [r1, r2, r3] = range;
+console.log(r1, r2, r3); // destructuring assignment : 10 20 30
