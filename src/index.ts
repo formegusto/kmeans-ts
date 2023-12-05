3031a3067dabfc0eaa70fe51400adc547775a02b
formegusto <formegusto@gmail.com>
2023-12-05 16:58:45 +0900

    set init centroids method

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import { KMeans } from "./models";
 import path from "path";
 import fs from "fs";
-import { euclideanDistance } from "./utils/similarity.util";
 
 const K = 4;
 const kmeans = new KMeans(K);
@@ -12,22 +11,5 @@ const dataset = JSON.parse(
   fs.readFileSync(`${filepath}/${filename}`, { encoding: "utf-8" })
 );
 
-const centroids = [];
-const ranIdx = Math.floor(Math.random() * dataset.length);
-const firstCentroid = dataset[ranIdx];
-centroids.push(firstCentroid);
-
-const distances = [];
-for (let data of dataset) {
-  let minDistance = Number.MAX_SAFE_INTEGER;
-  for (let centroid of centroids) {
-    const distance = euclideanDistance(data, centroid);
-    minDistance = Math.min(distance, minDistance);
-  }
-  distances.push(minDistance);
-}
-const maxDistance = Math.max.apply(null, distances);
-const maxIdx = distances.indexOf(maxDistance);
-const nextCentroid = dataset[maxIdx];
-centroids.push(nextCentroid);
-console.log(centroids);
+kmeans.setInitCentroids(dataset);
+console.log(kmeans.centroids);
--- src/models/kmeans.model.ts
+++ src/models/kmeans.model.ts
@@ -1,5 +1,35 @@
+import { euclideanDistance } from "../utils";
+
 export class KMeans implements IKMeans {
+  centroids?: number[][];
+
   constructor(public K: number) {}
 
-  setInitCentroids(dataset: number[][]) {}
+  setInitCentroids(dataset: number[][]) {
+    // 1.1 첫 번째 중심점 무작위 선정
+    const centroids = [];
+    const ranIdx = Math.floor(Math.random() * dataset.length);
+    const firstCentroid = dataset[ranIdx];
+    centroids.push(firstCentroid);
+
+    for (let k = 1; k < this.K; k++) {
+      const distances = [];
+      for (let data of dataset) {
+        let minDistance = Number.MAX_SAFE_INTEGER;
+        // 1.2 선정된 중심점과 데이터 간의 거리 계산
+        for (let centroid of centroids) {
+          const distance = euclideanDistance(data, centroid);
+          minDistance = Math.min(distance, minDistance);
+        }
+        distances.push(minDistance);
+      }
+      // 1.3 최소 중심점 거리 중에서 최대 거리를 갖는 점을 다음 중심으로 선정
+      const maxDistance = Math.max.apply(null, distances);
+      const maxIdx = distances.indexOf(maxDistance);
+      const nextCentroid = dataset[maxIdx];
+      centroids.push(nextCentroid);
+      // 1.4 설정된 K개 만큼의 중심점이 샘플링될 때 까지 1.2단계와 1.3단계 반복
+    }
+    this.centroids = centroids;
+  }
 }
