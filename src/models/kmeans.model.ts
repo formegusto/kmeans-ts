e01f36470a51d3f2b308458cef5399a19dd2e957
formegusto <formegusto@gmail.com>
2024-01-09 01:56:06 +0900

    kmeans method type update

--- src/models/kmeans.model.ts
+++ src/models/kmeans.model.ts
@@ -67,13 +67,16 @@ export type ActionFunctionAny<R> = (...args: any[]) => R;
 type KMeansMethod1<T1, R> = (t1: T1) => R;
 type KMeansMethod2<T1, T2, R> = (t1: T1, t2: T2) => R;
 type KMeansMethod3<T1, T2, T3, R> = (t1: T1, t2: T2, t3: T3) => R;
+type KMeansMethod<R, T1, T2 = never, T3 = never> = T3 extends never
+  ? KMeansMethod1<T1, R>
+  : any;
 
 interface IKMeans extends IKMeansSetting {
-  initCenters: KMeansMethod1<number[][], number[][]>;
-  calcDistances: KMeansMethod2<number[][], number[][], number[][]>;
-  setLabels: KMeansMethod1<number[][], number[]>;
-  moveCenters: KMeansMethod2<number[][], number[][], number[][]>;
-  calcInertia: KMeansMethod3<number[][], number[][], number[], number>;
+  initCenters: KMeansMethod<number[][], number[][]>;
+  calcDistances: KMeansMethod<number[][], number[][], number[][]>;
+  setLabels: KMeansMethod<number[][], number[]>;
+  moveCenters: KMeansMethod<number[][], number[][], number[][]>;
+  calcInertia: KMeansMethod<number[][], number[][], number[], number>;
 }
 
 export class KMeans implements IKMeans {
