e47ba23f949a457e148507fa8b8763069ae57136
formegusto <formegusto@gmail.com>
2024-01-10 04:58:02 +0900

    kmeans types documentation

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -1,5 +1,6 @@
 const Errors: { [key: string]: Error } = {
   NotSettingInitCentroids: new Error("초기 중심점이 설정되지 않았습니다."),
+  EmptyRequiredParameters: new Error("필수 매개 변수가 제공되지 않았습니다."),
 };
 
 export default Errors;
--- src/models/kmeans.model.ts
+++ src/models/kmeans.model.ts
@@ -1,108 +1,56 @@
-import Errors from "../errors";
-import { euclideanDistance } from "../utils";
-
 interface IKMeansSetting {
   K: number;
 }
 
-type KMeansFunctionTypeOne<R = any, T1 = any> = (t1: T1) => R;
-type KMeansFunctionTypeTwo<R = any, T1 = any, T2 = any> = (t1: T1, t2: T2) => R;
-type KMeansFunctionTypeThree<R = any, T1 = any, T2 = any, T3 = any> = (
-  t1: T1,
-  t2: T2,
-  t3: T3
-) => R;
-type KMeansFunction<R = any, T1 = any, T2 = any, T3 = any> =
-  | KMeansFunctionTypeOne<R, T1>
-  | KMeansFunctionTypeTwo<R, T1, T2>
-  | KMeansFunctionTypeThree<R, T1, T2, T3>;
+interface IKmeansMethodParams {
+  dataset?: number[][];
+  centers?: number[][];
+  distances?: number[][];
+  labels?: number[];
+}
 
-declare function stringOrNum<R>(p1: number[]): R;
-declare function stringOrNum<R>(p1: number[][], p2: number[][]): R;
-declare function stringOrNum<R>(
-  p1: number[][],
-  p2: number[][],
-  p3: number[]
-): R;
+interface IKMeansResult extends IKmeansMethodParams {
+  inertia: number;
+}
 
-/** argument inferring borrowed from lodash definitions */
-/*
-export function createAction<Payload>(
-    actionType: string,
-    payloadCreator: ActionFunction0<Payload>
-): ActionFunction0<Action<Payload>>;
-
-export function createAction<Payload, Arg1>(
-    actionType: string,
-    payloadCreator: ActionFunction1<Arg1, Payload>
-): ActionFunction1<Arg1, Action<Payload>>;
-
-export function createAction<Payload, Arg1, Arg2>(
-    actionType: string,
-    payloadCreator: ActionFunction2<Arg1, Arg2, Payload>
-): ActionFunction2<Arg1, Arg2, Action<Payload>>;
-
-export function createAction<Payload, Arg1, Arg2, Arg3>(
-    actionType: string,
-    payloadCreator: ActionFunction3<Arg1, Arg2, Arg3, Payload>
-): ActionFunction3<Arg1, Arg2, Arg3, Action<Payload>>;
-
-export function createAction<Payload, Arg1, Arg2, Arg3, Arg4>(
-    actionType: string,
-    payloadCreator: ActionFunction4<Arg1, Arg2, Arg3, Arg4, Payload>
-): ActionFunction4<Arg1, Arg2, Arg3, Arg4, Action<Payload>>;
-
-export function createAction<Payload>(
-    actionType: string
-): ActionFunction1<Payload, Action<Payload>>;
-
-export type ActionFunction0<R> = () => R;
-export type ActionFunction1<T1, R> = (t1: T1) => R;
-export type ActionFunction2<T1, T2, R> = (t1: T1, t2: T2) => R;
-export type ActionFunction3<T1, T2, T3, R> = (t1: T1, t2: T2, t3: T3) => R;
-export type ActionFunction4<T1, T2, T3, T4, R> = (t1: T1, t2: T2, t3: T3, t4: T4) => R;
-export type ActionFunctionAny<R> = (...args: any[]) => R;
-*/
-
-type KMeansMethod1<T1, R> = (t1: T1) => R;
-type KMeansMethod2<T1, T2, R> = (t1: T1, t2: T2) => R;
-type KMeansMethod3<T1, T2, T3, R> = (t1: T1, t2: T2, t3: T3) => R;
-type KMeansMethod<R, T1, T2 = never, T3 = never> = T3 extends never
-  ? KMeansMethod1<T1, R>
-  : any;
-
+type KMeansMethod<R = number[][]> = (params: IKmeansMethodParams) => R;
 interface IKMeans extends IKMeansSetting {
-  initCenters: KMeansMethod<number[][], number[][]>;
-  calcDistances: KMeansMethod<number[][], number[][], number[][]>;
-  setLabels: KMeansMethod<number[][], number[]>;
-  moveCenters: KMeansMethod<number[][], number[][], number[][]>;
-  calcInertia: KMeansMethod<number[][], number[][], number[], number>;
+  initCenters: KMeansMethod;
+  calcDistances: KMeansMethod;
+  setLabels: KMeansMethod<number[]>;
+  moveCenters: KMeansMethod;
+  calcInertia: KMeansMethod<number>;
+  fit: KMeansMethod<IKMeansResult>;
 }
 
 export class KMeans implements IKMeans {
   constructor(public K: number) {}
 
-  initCenters(dataset: number[][]): number[][] {
+  initCenters({ dataset }: IKmeansMethodParams): number[][] {
     return [];
   }
 
-  calcDistances(dataset: number[][], centers: number[][]): number[][] {
+  calcDistances({ centers }: IKmeansMethodParams): number[][] {
     return [];
   }
 
-  setLabels(distances: number[][]): number[] {
+  setLabels({ distances }: IKmeansMethodParams): number[] {
     return [];
   }
 
-  moveCenters(dataset: number[][], labels: number[][]): number[][] {
+  moveCenters({ dataset, labels }: IKmeansMethodParams): number[][] {
     return [];
   }
 
-  calcInertia(
-    dataset: number[][],
-    centers: number[][],
-    labels: number[]
-  ): number {
+  calcInertia({ dataset, centers, labels }: IKmeansMethodParams): number {
     return 0;
+  }
+
+  fit({ dataset }: IKmeansMethodParams): IKmeansMethodParams {
+    return {
+      centers: [],
+      labels: [],
+      inertia: 0,
+    };
   }
 }
