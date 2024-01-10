22c1d32925d19de02a7435df904e55c25c362b5c
formegusto <formegusto@gmail.com>
2024-01-11 05:55:53 +0900

    move centers example code

--- src/index.ts
+++ src/index.ts
@@ -21,13 +21,33 @@ setPrototype();
     // const K = 3;
     // const kmeans = new KMeans(K);
     // console.log(JSON.stringify(kmeans.fit(dataset)));
-    for (let i = 0; i < 4; i++) {
-      const dataset = generateRandomDataset({ shape: [100, 2] }) as number[][];
-      const K = 3;
-      const kmeans = new KMeans(K);
-      const centers = kmeans.initCenters({ dataset });
-      exportOutput(`process_1-2_test_${i}.json`, { dataset, centers });
+    // for (let i = 0; i < 4; i++) {
+    //   const dataset = generateRandomDataset({ shape: [100, 2] }) as number[][];
+    //   const K = 3;
+    //   const kmeans = new KMeans(K);
+    //   const centers = kmeans.initCenters({ dataset });
+    //   exportOutput(`process_1-2_test_${i}.json`, { dataset, centers });
+    // }
+
+    const dataset = generateRandomDataset({ shape: [100, 2] }) as number[][];
+    const K = 3;
+    const kmeans = new KMeans(K);
+    const steps = [];
+    let centers = kmeans.initCenters({ dataset });
+    while (true) {
+      const distances = kmeans.calcDistances({ dataset, centers });
+      const labels = kmeans.setLabels({ distances });
+      steps.push({
+        dataset,
+        centers,
+        labels,
+      });
+      const nextCenters = kmeans.moveCenters({ dataset, centers, labels });
+      if (!nextCenters) break;
+      centers = nextCenters;
     }
+    console.log(steps.length);
+    exportOutput(`process_3-4_test.json`, steps);
   } catch (err) {
     console.error(err);
   }
