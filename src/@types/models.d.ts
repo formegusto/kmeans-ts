33ae3437c1d8b84bfd5373eda108784660fc5413
formegusto <formegusto@gmail.com>
2024-01-15 03:41:01 +0900

    기존 kmeans iterable 코드 주석 및 삭제

--- src/@types/models.d.ts
+++ src/@types/models.d.ts
@@ -1,36 +1,33 @@
-declare interface IKMeansSetting {
+interface IKMeansSetting {
   K: number;
 }
 
-declare interface IKMeansIter extends IKMeansSetting {
-  dataset: number[][];
+interface IKMeansMethodParams {
+  dataset?: number[][];
+  centers?: number[][];
+  distances?: number[][];
+  labels?: number[];
 }
 
-declare interface IKMeansResult {
-  centroids: number[][];
-  labels: number[];
+interface IKMeansResult extends IKMeansMethodParams {
   inertia: number;
 }
 
-declare type IKMeansIteratorResult = IKMeansResult | undefined;
+declare interface IKMeans {}
 
-declare interface IKMeans extends IKMeansSetting {
-  fit: (dataset: number[][]) => IKMeansIteratorResult | null;
-}
+// declare interface IKMeansIterable
+//   extends IKMeansIter,
+//     Iterable<IKMeansIteratorResult, IKMeansIteratorResult> {
+//   setInitCentroids: (dataset: number[][]) => number[][];
+// }
 
-declare interface IKMeansIterable
-  extends IKMeansIter,
-    Iterable<IKMeansIteratorResult, IKMeansIteratorResult> {
-  setInitCentroids: (dataset: number[][]) => number[][];
-}
+// declare interface IKMeansIterator
+//   extends IKMeansIter,
+//     Iterator<IKMeansIteratorResult, IKMeansIteratorResult> {
+//   centroids?: number[][];
 
-declare interface IKMeansIterator
-  extends IKMeansIter,
-    Iterator<IKMeansIteratorResult, IKMeansIteratorResult> {
-  centroids?: number[][];
-
-  calcDistances: () => number[][];
-  labeling: (distances: number[][]) => number[];
-  calcCentroids: (labels: number[]) => void;
-  calcSSE: (labels: number[]) => number;
-}
+//   calcDistances: () => number[][];
+//   labeling: (distances: number[][]) => number[];
+//   calcCentroids: (labels: number[]) => void;
+//   calcSSE: (labels: number[]) => number;
+// }
