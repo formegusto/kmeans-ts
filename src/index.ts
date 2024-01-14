60b095197520a7f7bca50d6d8584767383c46d2f
formegusto <formegusto@gmail.com>
2024-01-14 09:32:10 +0900

    process description

--- src/index.ts
+++ src/index.ts
@@ -33,16 +33,21 @@ setPrototype();
     const K = 3;
     const kmeans = new KMeans(K);
     const steps = [];
+    // 1. K개의 초기 중심점 설정
     let centers = kmeans.initCenters({ dataset });
     while (true) {
+      // 2. 중심점과 데이터 간의 거리 계산
       const distances = kmeans.calcDistances({ dataset, centers });
+      // 3. 최소 거리 중심점의 군집 번호를 데이터에 부여
       const labels = kmeans.setLabels({ distances });
       steps.push({
         dataset,
         centers,
         labels,
       });
+      // 4. 군집 별 평균값을 계산하여 중심점에 반영
       const nextCenters = kmeans.moveCenters({ dataset, centers, labels });
+      // 5. 2~4의 과정을 중심점에 변화가 없을 때까지 반복
       if (!nextCenters) break;
       centers = nextCenters;
     }
