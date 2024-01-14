69ca2cde87520d569848f4bf74a845b139e06375
formegusto <formegusto@gmail.com>
2024-01-14 18:59:46 +0900

    fit update

--- src/index.ts
+++ src/index.ts
@@ -29,34 +29,47 @@ setPrototype();
     //   exportOutput(`process_1-2_test_${i}.json`, { dataset, centers });
     // }
 
-    const dataset = generateRandomDataset({ shape: [100, 2] }) as number[][];
-    const K = 3;
-    const kmeans = new KMeans(K);
-    const steps = [];
-    // 1. K개의 초기 중심점 설정
-    let centers = kmeans.initCenters({ dataset });
-    while (true) {
-      // 2. 중심점과 데이터 간의 거리 계산
-      const distances = kmeans.calcDistances({ dataset, centers });
-      // 3. 최소 거리 중심점의 군집 번호를 데이터에 부여
-      const labels = kmeans.setLabels({ distances });
-      // *. 평가
-      const inertia = kmeans.calcInertia({ dataset, centers, labels });
-      console.log(inertia);
-      steps.push({
+    // const dataset = generateRandomDataset({ shape: [100, 2] }) as number[][];
+    // const K = 3;
+    // const kmeans = new KMeans(K);
+    // const steps = [];
+    // // 1. K개의 초기 중심점 설정
+    // let centers = kmeans.initCenters({ dataset });
+    // while (true) {
+    //   // 2. 중심점과 데이터 간의 거리 계산
+    //   const distances = kmeans.calcDistances({ dataset, centers });
+    //   // 3. 최소 거리 중심점의 군집 번호를 데이터에 부여
+    //   const labels = kmeans.setLabels({ distances });
+    //   // *. 평가
+    //   const inertia = kmeans.calcInertia({ dataset, centers, labels });
+    //   console.log(inertia);
+    //   steps.push({
+    //     dataset,
+    //     centers,
+    //     labels,
+    //     inertia,
+    //   });
+    //   // 4. 군집 별 평균값을 계산하여 중심점에 반영
+    //   const nextCenters = kmeans.moveCenters({ dataset, centers, labels });
+    //   // 5. 2~4의 과정을 중심점에 변화가 없을 때까지 반복
+    //   if (!nextCenters) break;
+    //   centers = nextCenters;
+    // }
+    // console.log(steps.length);
+    // exportOutput(`process_3-4_test.json`, steps);
+
+    const results = [];
+    for (let i = 0; i < 8; i++) {
+      const dataset = generateRandomDataset({ shape: [100, 2] }) as number[][];
+      const K = 3 + Math.floor(Math.random() * 3);
+      const kmeans = new KMeans(K);
+      const result = kmeans.fit({ dataset });
+      results.push({
         dataset,
-        centers,
-        labels,
-        inertia,
+        result,
       });
-      // 4. 군집 별 평균값을 계산하여 중심점에 반영
-      const nextCenters = kmeans.moveCenters({ dataset, centers, labels });
-      // 5. 2~4의 과정을 중심점에 변화가 없을 때까지 반복
-      if (!nextCenters) break;
-      centers = nextCenters;
     }
-    console.log(steps.length);
-    exportOutput(`process_3-4_test.json`, steps);
+    exportOutput(`process_fit_test.json`, results);
   } catch (err) {
     console.error(err);
   }
--- src/models/kmeans.model.ts
+++ src/models/kmeans.model.ts
@@ -117,10 +117,20 @@ export class KMeans implements IKMeans {
   }
 
   fit({ dataset }: IKMeansMethodParams): IKMeansResult {
-    return {
-      centers: [],
-      labels: [],
-      inertia: 0,
-    };
+    // 1. K개의 초기 중심점 설정
+    let centers = this.initCenters({ dataset });
+    while (true) {
+      // 2. 중심점과 데이터 간의 거리 계산
+      const distances = this.calcDistances({ dataset, centers });
+      // 3. 최소 거리 중심점의 군집 번호를 데이터에 부여
+      const labels = this.setLabels({ distances });
+      // *. 평가
+      const inertia = this.calcInertia({ dataset, centers, labels });
+      // 4. 군집 별 평균값을 계산하여 중심점에 반영
+      const nextCenters = this.moveCenters({ dataset, centers, labels });
+      // 5. 2~4의 과정을 중심점에 변화가 없을 때까지 반복
+      if (!nextCenters) return { centers, labels, inertia };
+      centers = nextCenters;
+    }
   }
 }
